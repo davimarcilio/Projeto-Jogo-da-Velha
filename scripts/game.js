b47bd6d9ca847525beac113ada5a244f88e068c5
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-24 13:47:24 -0300

    código velha usando every

--- scripts/game.js
+++ scripts/game.js
@@ -67,12 +67,6 @@ function setPlayer() {
     return playerTime
 }
 function velha() {
-    // for (let i = 0; i < 9; i++){
-    //    if (board[i] != '') {
-    //    return true
-    //    } 
-    // }
-    // return false;
     if ( board.every((tabuleiro)=>{
         return tabuleiro != '';
      })) {
@@ -80,26 +74,4 @@ function velha() {
     } else {
         return false;
     }
-    // board.every((tabuleiro)=>{
-    //    return tabuleiro != '';
-    // })
-
-// board[0], [1], [2], [3], [4], [5], [6], [7], [8] == ''
-// if (board[0] != '' && board[1] != '' && board[2] != '' && board[3] != '' && board[4] != '' && board[5] != '' && board[6] != '' && board[7] != '' && board[8] != '') {
-//     return true;
-// }
-// return false;
 }
-// function verifyvelhawin() {
-//     for (let i = 0; i < winStates.length; i++) {
-//         const seq = winStates[i];
-
-//         let pos1 = seq[0];
-//         let pos2 = seq[1];
-//         let pos3 = seq[2];
-//         if ((board[pos1] == board[pos2]) && (board[pos1] == board[pos3]) && (board[pos1] != '')) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
